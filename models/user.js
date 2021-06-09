67e1f19ef93935c2c270e3f6ed86b685d0e919f2
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-09 12:53:18 -0400

    chore: add user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('./../db.js');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
