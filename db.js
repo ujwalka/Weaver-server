8e219515548b7eb7492cd51438b4debcd0e18f8a
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-09 12:17:11 -0400

    chore: add db connection

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.WS_MONGODB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(`Sorry, something went wrong! ${err}`);
+    } else {
+      console.log(`Database connected`);
+    }
+  }
+);
+
+module.exports = mongoose;
